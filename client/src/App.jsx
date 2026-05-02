48285a797c0d52a1e3324e6ee9d1a96bbe3022f6
Asghar <bunyanalmarsoos2@gmail.com>
2026-05-02 22:47:41 +0500

    Add GalleryPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Layout/Navbar/Navbar";
 import Footer from "./components/Layout/Footer/Footer";
+import GalleryPage from "./pages/Gallery/GalleryPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
       <Footer/>
     </>
---
+++ client/src/pages/Gallery/GalleryData.js
@@ -0,0 +1,58 @@
+export const photos = [
+  {
+    src: "https://images.unsplash.com/photo-1586773860418-d37222d8fce3",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1579684385127-1ef15d508118",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1584515933487-779824d29309",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1580281657527-47f249e8f4df",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1538108149393-fbbd81895907",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1582750433449-648ed127bb54",
+    width: 800,
+    height: 600
+  },
+  
+  {
+    src: "https://images.unsplash.com/photo-1551601651-2a8555f1a136",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1587351021759-3e566b6af7cc",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1576091160550-2173dba999ef",
+    width: 800,
+    height: 600
+  },
+  {
+    src: "https://images.unsplash.com/photo-1581594693702-fbdc51b2763b",
+    width: 800,
+    height: 600
+  }
+];
